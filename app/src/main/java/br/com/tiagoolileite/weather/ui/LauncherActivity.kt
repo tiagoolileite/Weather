4ad32ce1e1b3d264ffa73dc35fad9fddb38e1a03
Tiago Oliveira Leite <tiago.olileite@gmail.com>
2022-09-20 00:53:53 -0300

    Configuração da Splash Screen
    - Adição do vetor do logo
    - Criação de Handler da splash
    - Edição de estilo base do app
    - Criação da MainActivity

--- app/src/main/java/br/com/tiagoolileite/weather/ui/LauncherActivity.kt
+++ app/src/main/java/br/com/tiagoolileite/weather/ui/LauncherActivity.kt
@@ -1,12 +1,23 @@
 package br.com.tiagoolileite.weather.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
 import br.com.tiagoolileite.weather.R
 
 class LauncherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launcher)
+
+        openMainActivityWithHandler()
+    }
+
+    private fun openMainActivityWithHandler() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+        }, 2500)
     }
 }
